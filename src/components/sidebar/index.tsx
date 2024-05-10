1822d4b603b9fed4b7a0beb9bab0aa334351f896
issam eljaouhary <iseljao89@gmail.com>
2024-05-10 16:34:37 +0100

    feat: dev page

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -57,13 +57,13 @@ export const Sidebar = () => {
   return (
     <div
       data-sidebar-open="false"
-      className="dark fixed inset-y-0 left-0 w-sidebar-width shrink-0 border-r bg-card  text-card-foreground transition-[width] duration-300 ease-in-out hover:w-sidebar-open"
+      className="dark fixed inset-y-0 left-0 z-[11] w-sidebar-width shrink-0 border-r bg-card  text-card-foreground transition-[width] duration-300 ease-in-out hover:w-sidebar-open"
     >
       <div className="relative h-header border-b">
         <div className=" grid h-full w-sidebar-closed place-content-center font-extrabold">
           LOGO
         </div>
-        <SidebarToggleButton />
+        {/* <SidebarToggleButton /> */}
         <div className="flex flex-col px-4">
           {routes.map((route) => (
             <RouteItem key={route.name} route={route} />
